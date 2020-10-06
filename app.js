27e37a6f42178500ab60158823f2c5d2ace2207d
Daniel Fewster <daniel.fewster@hotmail.com>
2020-10-06 16:48:17 +1000

    Removed Pug view engine and prep for React

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,9 +16,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-app.get('/', (req, res) => { 
-    res.render('index'); 
-});
 
 module.exports = app;
