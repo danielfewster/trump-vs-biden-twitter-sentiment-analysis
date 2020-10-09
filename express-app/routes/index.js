09eb9417e6bf9e9a05ad8f69908aceb578e018b2
Daniel Fewster <daniel.fewster@hotmail.com>
2020-10-10 01:22:10 +1000

    Added some starting experimental code to express-app

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,5 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const natural = require("natural");
+const Twitter = require('twitter');
+
+const user = new Twitter({ // NOTE: using app auth here because apparently it has higher rate limits (I think this means query limit)
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+});
+
+const response = await user.getBearerToken();
+const app = new Twitter({
+    bearer_token: response.access_token
+});
 
 router.get('/', (req, res) => {
     
