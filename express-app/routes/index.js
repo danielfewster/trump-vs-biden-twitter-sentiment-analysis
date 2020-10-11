80715651e7d0486d248a6f2b10a35cc74cf06048
Daniel Fewster <daniel.fewster@hotmail.com>
2020-10-11 23:38:42 +1000

    Added Redis+S3 Persistence

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -8,24 +8,74 @@ const Analyzer = require('natural').SentimentAnalyzer;
 const stemmer = require('natural').PorterStemmer;
 const analyzer = new Analyzer("English", stemmer, "afinn");
 
-router.get('/search', (req, res) => {
-    const url = "https://api.twitter.com/2/tweets/search/recent";
+const asyncRedis = require("async-redis"); // NOTE: async wrapper package for redis package
+const client = asyncRedis.createClient();
+client.on("error", err => console.error(err));
 
-    return axios.get(url, {
-        headers: {
-            "Authorization" : "Bearer " + process.env.TWITTER_BEARER_TOKEN
-        },
-        params: {
-            query: "trump",
-            max_results: "100"
-        }
-    })
-    .then(searchRes => searchRes.data.data)
-    .then(searchRes => {
+const AWS = require('aws-sdk');
+AWS.config.getCredentials(err => { if (err) console.log(err.stack); });
+
+function getQueryResult(query) {
+    const S3 = new AWS.S3({ apiVersion: '2006-03-01' });
+    const bucketName = "a2-twitter-feed-bucket";
+    const S3Key = query;
+    const redisKey = `twitterSearchRes:${query}`;
+    const redisSecondsToExpire = 60 * 60 * 3;
+
+    return client.get(redisKey)
+        .then(redisRes => {
+            if (redisRes) return JSON.parse(redisRes);
+            else {
+                const getS3Result = S3.getObject({
+                    Bucket: bucketName,
+                    Key: S3Key
+                }).promise();
+                
+                return getS3Result.then(s3Res => {
+                    const resultJSON = JSON.parse(s3Res.Body);
+                    client.setex(redisKey, redisSecondsToExpire, JSON.stringify(resultJSON)).then(succ => {
+                        if (succ) console.log('Successfully uploaded data to Redis at ' + redisKey);
+                    }).catch(err => console.log(err)); 
+                    return resultJSON; 
+                })
+                .catch(() => {
+                    return axios.get("https://api.twitter.com/2/tweets/search/recent", { 
+                        headers: {
+                            "Authorization" : "Bearer " + process.env.TWITTER_BEARER_TOKEN
+                        },
+                        params: {
+                            query: query,
+                            max_results: "100"
+                        }
+                    })
+                    .then(searchRes => {
+                        const resultJSON = JSON.stringify(searchRes.data.data);
+
+                        client.setex(redisKey, redisSecondsToExpire, resultJSON).then(succ => {
+                            if (succ) console.log('Successfully uploaded data to Redis at ' + redisKey);
+                        }).catch(err => console.log(err)); 
+
+                        S3.putObject({ 
+                            Bucket: bucketName, Key: S3Key, Body: resultJSON 
+                        }).promise().then(() => {
+                            console.log('Successfully uploaded data to S3 Bucket at ' + bucketName + '/' + S3Key);
+                        }).catch(err => console.error(err));
+
+                        return searchRes.data.data;
+                    })
+                    .catch(err => console.error(err));
+                });
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+router.get('/search/:query', (req, res) => {
+    getQueryResult(req.params.query).then(searchData => {
         let wordsFromTweets = [];
         let tweetsWithSentiment = [];
 
-        searchRes.forEach(tweet => {
+        searchData.forEach(tweet => {
             const wordsFromText = tokenizer.tokenize(tweet.text);
             tweetsWithSentiment.push({
                 id: tweet.id,
